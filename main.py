ad8d5137c4506849b6cda6cced1ef8e7f1a1a05c
Ilan Eliashberg <sendm2i@gmail.com>
2025-11-11 22:04:29 -0800

    doing first inference, is really bad, possible data normalization and scaling issues

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import mediapipe as mp
 import cv2
 import time
 import torch
+import numpy as np
+from static_gesture_net import StaticGestureNet
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -10,6 +12,11 @@ mp_hands = mp.solutions.hands
 cap = cv2.VideoCapture(0)  # could be 2 or something else
 prev_time = time.perf_counter()  # for fps
 
+# define the model we will be using
+model = torch.load("static_gesture_net_v1.pt", map_location="cpu")
+net = StaticGestureNet(model["input_dim"], model["num_classes"])
+net.load_state_dict(model["model_state_dict"])
+classes = model["classes"]
 with mp_hands.Hands(
     model_complexity=0, min_detection_confidence=0.8, min_tracking_confidence=0.5, max_num_hands = 1
 ) as hands:
@@ -32,6 +39,8 @@ with mp_hands.Hands(
         image = cv2.cvtColor(
             image, cv2.COLOR_RGB2BGR
         )  # need to convert back to BGR default for open cv to display
+
+        prediction = None
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
@@ -41,7 +50,23 @@ with mp_hands.Hands(
                     mp_drawing_styles.get_default_hand_landmarks_style(),
                     mp_drawing_styles.get_default_hand_connections_style(),
                 )
+
+            # actual model inference
+            curr_feature_vec = []
+            for lm in results.multi_hand_landmarks[0].landmark:
+                curr_feature_vec.extend([lm.x, lm.y, lm.z])
+
+            # flattened feature vec (ready to go in model)
+            x = torch.from_numpy(np.array(curr_feature_vec, dtype=np.float32)).unsqueeze(0)  # shape [1, 63]
+
+            with torch.no_grad():
+                net.eval()
+                outputs= net(x)
+                _, pred_ndx = torch.max(outputs, 1)
+                prediction = classes[pred_ndx.item()]
+
         flipped_img = cv2.flip(image, 1)
+
 
         #putting fps on the screen
         curr_time = time.perf_counter()
@@ -59,6 +84,9 @@ with mp_hands.Hands(
             (255, 0, 0),
             4,
         )
+
+        if prediction is not None:
+            cv2.putText(flipped_img, f"{prediction}", (10,80), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), thickness = 4)
 
         cv2.imshow("Hand Tracking", flipped_img)
         if cv2.waitKey(1) & 0xFF == ord("q"):
--- train_model.py
+++ train_model.py
@@ -8,71 +8,99 @@ import torch.nn as nn
 import torch.optim as optim
 from tqdm.auto import tqdm
 import matplotlib.pyplot as plt
+from static_gesture_net import StaticGestureNet
 
 
 def load_and_split(filename):
     df = pd.read_csv(filename)
     features = df.iloc[:, :-1].values.astype(np.float32)
     labels = df.iloc[:, -1].values  # currently still string labels
-    encoder = LabelEncoder()    # create instance of the label encoder
-    labels_encoded = encoder.fit_transform(labels)  #actually encode the string labels
-    features_train, features_temp, labels_train, labels_temp = train_test_split(features, labels_encoded, test_size = 0.3, stratify = labels_encoded, random_state = 42) # split the data 70/30 into train/temp
-    features_test, features_validation, labels_test, labels_validation = train_test_split(features_temp, labels_temp, test_size=0.5, stratify=labels_temp, random_state = 42) # split the temp into 50/50 train/validation
+    encoder = LabelEncoder()  # create instance of the label encoder
+    labels_encoded = encoder.fit_transform(labels)  # actually encode the string labels
+    features_train, features_temp, labels_train, labels_temp = train_test_split(
+        features,
+        labels_encoded,
+        test_size=0.3,
+        stratify=labels_encoded,
+        random_state=42,
+    )  # split the data 70/30 into train/temp
+    features_test, features_validation, labels_test, labels_validation = (
+        train_test_split(
+            features_temp,
+            labels_temp,
+            test_size=0.5,
+            stratify=labels_temp,
+            random_state=42,
+        )
+    )  # split the temp into 50/50 train/validation
 
-    return features_train, features_validation, features_test, labels_train, labels_validation, labels_test, encoder
+    return (
+        features_train,
+        features_validation,
+        features_test,
+        labels_train,
+        labels_validation,
+        labels_test,
+        encoder,
+    )
 
 
-def train_model(features_train, features_validation, labels_train, labels_validation, encoder, num_epochs):
-    train_ds = TensorDataset(torch.from_numpy(features_train).float(),
-                         torch.from_numpy(labels_train).long())
+def train_model(
+    features_train,
+    features_validation,
+    labels_train,
+    labels_validation,
+    encoder,
+    num_epochs,
+):
+    train_ds = TensorDataset(
+        torch.from_numpy(features_train).float(), torch.from_numpy(labels_train).long()
+    )
     train_loader = DataLoader(train_ds, batch_size=4, shuffle=True)
 
-    validation_ds = TensorDataset(torch.from_numpy(features_validation).float(), torch.from_numpy(labels_validation).long())
+    validation_ds = TensorDataset(
+        torch.from_numpy(features_validation).float(),
+        torch.from_numpy(labels_validation).long(),
+    )
 
-    validation_loader = DataLoader(validation_ds, batch_size = 4, shuffle = False)
+    validation_loader = DataLoader(validation_ds, batch_size=4, shuffle=False)
 
-    class StaticGestureNet(nn.Module):
-        def __init__(self, input_dimension, num_classes):
-            super().__init__()
-            self.model = nn.Sequential(
-            nn.Linear(input_dimension, 128),
-            nn.ReLU(),
-            nn.Linear(128, 64),
-            nn.ReLU(),
-            nn.Linear(64, num_classes)
-        )
-        def forward(self, x):
-            return self.model(x)
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")   #use GPU for training if available. Otherwise CPU
+    device = torch.device(
+        "cuda" if torch.cuda.is_available() else "cpu"
+    )  # use GPU for training if available. Otherwise CPU
 
     # create an instance of our neural network class
     net = StaticGestureNet(features_train.shape[1], len(encoder.classes_))
-    net.to(device) # move our network instance to the gpu if we have one
+    net.to(device)  # move our network instance to the gpu if we have one
 
-    criterion = nn.CrossEntropyLoss()   # set our loss function
-    optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)  # set our optimizer
+    criterion = nn.CrossEntropyLoss()  # set our loss function
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)  # set our optimizer
 
     training_accuracy = []
     validation_accuracy = []
     for epoch in range(num_epochs):
-        progress_bar = tqdm(total=len(train_loader), desc=f"epoch {epoch+1}", unit="batch")
+        progress_bar = tqdm(
+            total=len(train_loader), desc=f"epoch {epoch+1}", unit="batch"
+        )
 
-
-        net.train() # using batch norm and dropout
+        net.train()  # using batch norm and dropout
         num_correct_train = 0
         num_total_train = 0
         for X, y in train_loader:
             optimizer.zero_grad()
-            X,y = X.to(device), y.to(device)
+            X, y = X.to(device), y.to(device)
 
-            outputs = net(X)    # forward pass and calculates outputs
-            loss = criterion(outputs, y)    # get cross entropy loss
-            loss.backward() # autograd calculates all the gradients for us
-            optimizer.step() # updates the model weights using the graidents and current model params
+            outputs = net(X)  # forward pass and calculates outputs
+            loss = criterion(outputs, y)  # get cross entropy loss
+            loss.backward()  # autograd calculates all the gradients for us
+            optimizer.step()  # updates the model weights using the graidents and current model params
 
-            _, predicted = torch.max(outputs, 1)    # get the argmax of ouputs and set that to predicted
-            num_correct_train += (predicted == y).sum().item()  # sum the amount of correct
+            _, predicted = torch.max(
+                outputs, 1
+            )  # get the argmax of ouputs and set that to predicted
+            num_correct_train += (
+                (predicted == y).sum().item()
+            )  # sum the amount of correct
             num_total_train += len(y)
             progress_bar.update(1)  # update the progress bar
         training_accuracy.append(num_correct_train / num_total_train)
@@ -81,14 +109,27 @@ def train_model(features_train, features_validation, labels_train, labels_valida
         num_total_validation = 0
         with torch.no_grad():
             for X, y in validation_loader:
-                net.eval()    # turing off batch norm and dropout for evaulation
-                X, y = X.to(device), y.to(device)   # move inputs and labels to the actual device (gpu if using)
-                outputs = net(X)    # get the 1d vector of likelihoods for each class
-                _, predicted = torch.max(outputs, 1)    # take the argmax of each class for each output and set that to predicted
+                net.eval()  # turing off batch norm and dropout for evaulation
+                X, y = X.to(device), y.to(
+                    device
+                )  # move inputs and labels to the actual device (gpu if using)
+                outputs = net(X)  # get the 1d vector of likelihoods for each class
+                _, predicted = torch.max(
+                    outputs, 1
+                )  # take the argmax of each class for each output and set that to predicted
                 num_correct_validation += (predicted == y).sum().item()
                 num_total_validation += len(y)
         validation_accuracy.append(num_correct_validation / num_total_validation)
     progress_bar.close()
+
+    # after loop: write one file
+    checkpoint = {
+        "model_state_dict": net.state_dict(),
+        "input_dim": features_train.shape[1],
+        "num_classes": len(encoder.classes_),
+        "classes": encoder.classes_.tolist(),
+    }
+    torch.save(checkpoint, "static_gesture_net_v1.pt")
 
     epochs = range(1, num_epochs + 1)
     plt.figure()
@@ -100,12 +141,29 @@ def train_model(features_train, features_validation, labels_train, labels_valida
     plt.grid(True)
     plt.show()
     print(validation_accuracy)
-   # need to return the parameters of the model or save them somehow
+
+
+# need to return the parameters of the model or save them somehow
+
 
 def main():
-    features_train, features_validation, features_test, labels_train, labels_validation, labels_test, encoder = load_and_split("static_dataset_copy.csv")
-    train_model(features_train, features_validation, labels_train, labels_validation, encoder, 50)
-
+    (
+        features_train,
+        features_validation,
+        features_test,
+        labels_train,
+        labels_validation,
+        labels_test,
+        encoder,
+    ) = load_and_split("static_dataset_copy.csv")
+    train_model(
+        features_train,
+        features_validation,
+        labels_train,
+        labels_validation,
+        encoder,
+        30,
+    )
 
 if __name__ == "__main__":
     main()
